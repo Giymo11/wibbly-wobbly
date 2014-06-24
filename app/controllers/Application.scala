76e2673aa3a8daa34c570e3018adee9952896a3c
Giymo11 <gizmo.head@yahoo.de>
2014-06-24 04:23:12 +0200

    Added some hard-coded sample, included bootstrap, played with the template engine

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,7 +1,7 @@
 package controllers
 
-import play.api._
 import play.api.mvc._
+import play.twirl.api.Html
 
 object Application extends Controller {
 
@@ -10,7 +10,11 @@ object Application extends Controller {
   }
 
   def index = Action {
-    Ok("Hello World!")
+    Ok(views.html.main("Hello World")(Html("<p>Hello World</p>")))
+  }
+
+  def home = Action {
+    Ok(views.html.home("Wibbly Wobbly Startpage"))
   }
 
 }
---
+++ app/models/User.scala
@@ -0,0 +1,8 @@
+package models
+
+/**
+ * Created by Gizmo on 2014-06-24.
+ */
+class User(val username: String, val password: String) {
+
+}
