f36192c926a2f85719a4fd2d32c875e1c7bec6ac
Giymo11 <gizmo.head@yahoo.de>
2014-06-26 01:55:08 +0200

    Uses Polymer as front-end framework.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,19 +1,23 @@
 package controllers
 
 import play.api.mvc._
-import play.twirl.api.Html
 
 object Application extends Controller {
 
   def help = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.help("Your new application is ready."))
   }
 
   def index = Action {
-    Ok(views.html.main("Hello World")(Html("<p>Hello World</p>")))
+    Ok(views.html.index())
+    //Ok(views.html.main("Hello World")(Html("<p>Hello World</p>")))
   }
 
   def home = Action {
     Ok(views.html.home("Wibbly Wobbly Startpage"))
+  }
+
+  def polymer_home = Action {
+    Ok(views.html.polymer_home("Card 1", "Card 2", "Card 3"))
   }
 }
